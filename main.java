4a58c86a4b85723ee71fab7f12917a43ec790699
Shrinu Kushagra <shrinuk91@gmail.com>
2022-07-09 21:44:42 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Dcoder!");
+    int a=30,b=40;
+    float c=72.98f;
+    System.out.println(a+b+c);
+   }
+ }
+// use f in float,d in double, l in long datatype
