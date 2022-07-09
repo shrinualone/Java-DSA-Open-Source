02179c6069df8f5c28f0274992dd35d3618daec6
Shrinu Kushagra <shrinuk91@gmail.com>
2022-07-09 23:08:25 +0530

    Add files via upload

---
+++ 2Literals.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Hello, Dcoder!");
+    byte bt = 2;
+    int it = 62;
+    short st = 7289;
+    long lg = 7199176199l;
+    char ch = 'S';
+    float ft = 77.33f;
+    double db = 8292.9843d;
+    boolean bl = true;
+    String str = "Shrinu";
+    System.out.println(str);
+   }
+ }
+// ln is used in print to make a new line
+// Use l in long, f in float, d in double,
+// "" in string
